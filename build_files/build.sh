92336941ed34a14a656309ce307952ec6821e277
XenHat <me@xenh.at>
2025-12-23 03:52:34 -0500

    fix bad build script syntax and remove a repo
    I'm tired, boss

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,28 +5,12 @@ set -ouex pipefail
 # Enable Copr support
 dnf5 install -y dnf5-plugins
 
-# Add more repositories
-dnf5 config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:/mkittler/Fedora_43/home:mkittler.repo
-
 # Remove KDE Entirely
-RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
-  --mount=type=cache,dst=/var/cache \
-  --mount=type=cache,dst=/var/log \
-  --mount=type=tmpfs,dst=/tmp \
-  dnf5 remove -y plasma* kde* kf5* kf6* libwayland-client libx11 &&
-  dnf5 autoremove -y
+dnf5 remove -y plasma* kde* kf5* kf6* libwayland-client libx11
+dnf5 autoremove -y
 
 # Install wireless support
-RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
-  --mount=type=cache,dst=/var/cache \
-  --mount=type=cache,dst=/var/log \
-  --mount=type=tmpfs,dst=/tmp \
-  dnf5 install -y NetworkManager-wifi NetworkManager-wwan wpa_supplicant wireless-regdb NetworkManager-tui
+dnf5 install -y NetworkManager-wifi NetworkManager-wwan wpa_supplicant wireless-regdb NetworkManager-tui
 
 # Install linux-firmware
-RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
-  --mount=type=cache,dst=/var/cache \
-  --mount=type=cache,dst=/var/log \
-  --mount=type=tmpfs,dst=/tmp \
-  dnf5 install -y linux-firmware &&
-  dnf5 install -y kmod-nvidia ublue-os-nvidia-addons nvidia-driver nvidia-settings nvidia-gpu-firmware
+dnf5 install -y linux-firmware kmod-nvidia ublue-os-nvidia-addons nvidia-driver nvidia-settings nvidia-gpu-firmware
