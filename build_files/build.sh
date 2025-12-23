1bdb745e586224c85a9cf654fc3e61441e50c913
XenHat <me@xenh.at>
2025-12-23 04:27:18 -0500

    revert too-aggressive cleanup

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,7 +6,9 @@ set -ouex pipefail
 dnf5 install -y dnf5-plugins
 
 # Remove KDE Entirely
-dnf5 remove -y plasma* kde* kf5* kf6* libwayland-client libX11
+dnf5 remove -y plasma* kde* kf5* kf6*
+# not stable yet, this removes too much
+# dnf5 remove -y libwayland-client libX11
 dnf5 autoremove -y
 
 # Install wireless support
