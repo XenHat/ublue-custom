22e5e93f8f70e7ea6b02af90f69d3f519c83cfc8
XenHat <me@xenh.at>
2025-12-23 04:08:20 -0500

    try to get this building tonight

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,6 +11,3 @@ dnf5 autoremove -y
 
 # Install wireless support
 dnf5 install -y NetworkManager-wifi NetworkManager-wwan wpa_supplicant wireless-regdb NetworkManager-tui
-
-# Install linux-firmware
-dnf5 install -y linux-firmware kmod-nvidia ublue-os-nvidia-addons nvidia-driver nvidia-settings nvidia-gpu-firmware
